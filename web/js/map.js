c139c9eacd323a0aa99fd26d72923f172fc9b3ff
Bilal Karim <bilalkarim@Bilals-MacBook-Pro.local>
2014-11-22 15:12:11 -0700

    Initialized map and added the app’s UI

---
+++ web/js/map.js
@@ -0,0 +1,27 @@
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 12,
+	minZoom: 11,
+    center: new google.maps.LatLng(51.0453246, -114.0581012),
+	panControl: false,
+	mapTypeControl: false,
+    /*mapTypeControlOptions: {
+		style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+		position: google.maps.ControlPosition.BOTTOM_LEFT
+    },*/
+    zoomControl: false,
+    zoomControlOptions: {
+       	style: google.maps.ZoomControlStyle.SMALL,
+	   	position: google.maps.ControlPosition.RIGHT_BOTTOM
+   	},
+	streetViewControl: false
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var useragent = navigator.userAgent;
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+	  map.setZoom(10);
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
