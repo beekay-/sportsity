2c0b5bca034b66e2b86a30b4e1138d6d4fc23814
Bilal Karim <bilalkarim@Bilals-MacBook-Pro.local>
2014-11-22 23:03:19 -0700

    Code cleanup & added clickToClose to jQuery dialog

--- web/js/map.js
+++ web/js/map.js
@@ -1,15 +1,12 @@
 var map;
+
 function initialize() {
-  var mapOptions = {
+    var mapOptions = {
     zoom: 12,
 	minZoom: 11,
     center: new google.maps.LatLng(51.0453246, -114.0581012),
 	panControl: false,
 	mapTypeControl: false,
-    /*mapTypeControlOptions: {
-		style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-		position: google.maps.ControlPosition.BOTTOM_LEFT
-    },*/
     zoomControl: false,
     zoomControlOptions: {
        	style: google.maps.ZoomControlStyle.SMALL,
@@ -17,17 +14,17 @@ function initialize() {
    	},
 	streetViewControl: false
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  var useragent = navigator.userAgent;
-  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
-	  map.setZoom(10);
-  }
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var useragent = navigator.userAgent;
+    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+	  map.setZoom(11);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
-
 function modalUI() {
-	var options = { autoOpen: false, modal: true, width: '80%', height: 'auto', resizable: false, closeOnEscape: true };
+	var options = {show: {effect: "bounce", duration: 1000}, modal: true, width: '275', height: '275', resizable: false, closeOnEscape: true, open: function() {jQuery('.ui-widget-overlay').bind('click', function() { jQuery('#about').dialog('close');})} };
+    var optionsHelp = {show: {effect: "bounce", duration: 1000}, modal: true, width: '275', height: '275', resizable: false, closeOnEscape: true, open: function() {jQuery('.ui-widget-overlay').bind('click', function() { jQuery('#help').dialog('close');})} };
     $('.open-about').click(function() { $('#about').dialog(options).dialog('open'); });
-	$('.open-help').click(function() { $('#help').dialog(options).dialog('open'); });
+	$('.open-help').click(function() { $('#help').dialog(optionsHelp).dialog('open'); });
 }
