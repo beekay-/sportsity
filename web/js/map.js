4c321aca17e211e1a053ea4e67d4bc42f1c8549d
Bilal Karim <beekay-@users.noreply.github.com>
2014-11-23 19:38:40 -0700

    Removed jQuery Touch Punch and optimized jQuery UI
    Also added a fade effect when jQuery dialog closes.

--- web/js/map.js
+++ web/js/map.js
@@ -3,27 +3,27 @@ var map;
 function initialize() {
     var mapOptions = {
     zoom: 12,
-	minZoom: 11,
+    minZoom: 11,
     center: new google.maps.LatLng(51.0453246, -114.0581012),
-	panControl: false,
-	mapTypeControl: false,
+    panControl: false,
+    mapTypeControl: false,
     zoomControl: false,
     zoomControlOptions: {
-       	style: google.maps.ZoomControlStyle.SMALL,
-	   	position: google.maps.ControlPosition.RIGHT_BOTTOM
-   	},
-	streetViewControl: false
-  };
+        style: google.maps.ZoomControlStyle.SMALL,
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+    },
+    streetViewControl: false
+    };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     var useragent = navigator.userAgent;
-    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
-	  map.setZoom(11);
-    }
+        if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+            map.setZoom(11);
+        }
     google.maps.event.addDomListener(window, 'load', initialize);
 }
 
 function modalUIOpen() {
-	var options = {show: {effect: "bounce", duration: 1000}, modal: true, width: 275, minHeight: 255, height: 'auto', resizable: false, closeOnEscape: true, open: function() {jQuery('.ui-widget-overlay').bind('click', function() { jQuery('#about').dialog('close');})} };
+	var options = {show: {effect: "bounce", duration: 1000}, modal: true, width: 275, minHeight: 255, height: 'auto', resizable: false, closeOnEscape: true, hide: { effect: "fade", duration: 200 }, open: function() {jQuery('.ui-widget-overlay').bind('click', function() { jQuery('#about').dialog('close');})} };
     var optionsHelp = {show: {effect: "bounce", duration: 1000}, modal: true, width: '275', height: '275', resizable: false, closeOnEscape: true, open: function() {jQuery('.ui-widget-overlay').bind('click', function() { jQuery('#help').dialog('close');})} };
     $('.open-about').click(function() { $('#about').dialog(options).dialog('open'); });
 	$('.open-help').click(function() { $('#help').dialog(optionsHelp).dialog('open'); });
