bfa383b2c22e97d66190fc078e78bc05585a3fbe
Bilal Karim <beekay-@users.noreply.github.com>
2014-11-30 15:05:55 -0700

    Added HTML5 geolocation API.

--- web/js/map.js
+++ web/js/map.js
@@ -23,12 +23,24 @@ function initialize() {
 }
 
 function modalUIOpen() {
-    var options = {show: {effect: "bounce", duration: 1000}, modal: true, width: 275, minHeight: 255, height: 'auto', resizable: false, closeOnEscape: true, hide: { effect: "fade", duration: 200 }, open: function() {jQuery('.ui-widget-overlay').bind('click', function() { jQuery('#about').dialog('close');})} };
+	var options = {show: {effect: "bounce", duration: 1000}, modal: true, width: 275, minHeight: 255, height: 'auto', resizable: false, closeOnEscape: true, hide: { effect: "fade", duration: 200 }, open: function() {jQuery('.ui-widget-overlay').bind('click', function() { jQuery('#about').dialog('close');})} };
     var optionsHelp = {show: {effect: "bounce", duration: 1000}, modal: true, width: '275', height: '275', resizable: false, closeOnEscape: true, open: function() {jQuery('.ui-widget-overlay').bind('click', function() { jQuery('#help').dialog('close');})} };
     $('.open-about').click(function() { $('#about').dialog(options).dialog('open'); });
-    $('.open-help').click(function() { $('#help').dialog(optionsHelp).dialog('open'); });
+	$('.open-help').click(function() { $('#help').dialog(optionsHelp).dialog('open'); });
 }
 
 function modalUIClose() {
     $('#about').dialog('close');   
+}
+
+function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            userLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            map.setZoom(15);
+            map.setCenter(userLocation);
+        }); 
+    } else {
+        alert("Sorry, geolocation not supported by your browser.");
+    }
 }
