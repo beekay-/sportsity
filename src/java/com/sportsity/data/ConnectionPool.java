f73b859059204d79288d595078b23c0e0fada307
ruskam <rustamkamberov@Rustams-MacBook-Air.local>
2015-01-25 19:29:38 -0700

    Connection Pool class
    - Added a connection non-argument constructor

--- src/java/com/sportsity/data/ConnectionPool.java
+++ src/java/com/sportsity/data/ConnectionPool.java
@@ -5,10 +5,27 @@
  */
 package com.sportsity.data;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 /**
  *
  * @author rustamkamberov
  */
 public class ConnectionPool {
+    
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+    
+    private ConnectionPool() {
+        try {
+            InitialContext initialContext = new InitialContext();
+            dataSource = (DataSource) initialContext.lookup("java:/comp/env/jdbc/sportsity");
+        }
+        catch (NamingException e) {
+            System.out.println(e);
+        }
+    }
     
 }
