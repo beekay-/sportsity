4d6402a53de5dbd4ac9aea6d1f3d702e8730544d
ruskam <rustam.kamberov@gmail.com>
2015-01-26 22:23:49 -0700

    Started VenueDB

--- src/java/com/sportsity/data/DBUtil.java
+++ src/java/com/sportsity/data/DBUtil.java
@@ -5,10 +5,45 @@
  */
 package com.sportsity.data;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author rustamkamberov
  */
 public class DBUtil {
+    
+    public static void closeStatement(Statement s) {
+        try {
+            if (s != null) {
+                s.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    
+    }
+    
+    public static void closePreparedStatement(Statement ps) {
+        try {
+           if (ps != null) {
+                ps.close();
+           }    
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
     
 }
--- src/java/com/sportsity/data/VenueDB.java
+++ src/java/com/sportsity/data/VenueDB.java
@@ -5,10 +5,46 @@
  */
 package com.sportsity.data;
 
+import com.sportsity.bean.Venue;
+import com.sportsity.bean.VenueSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author rustamkamberov
  */
 public class VenueDB {
+    
+    public static VenueSet getVenues(String venueType) {
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        Connection connection = connectionPool.getConnection();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        
+        String query = "SELECT * "
+                        + "FROM sports as s "
+                        + "WHERE s.venuetype = ? "
+                        + "ORDER BY c.venueid;";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, venueType);
+            resultSet = preparedStatement.executeQuery();
+            VenueSet venueSet = new VenueSet();
+            while (resultSet.next()) {
+                Venue venue = new Venue();
+                venue.setVenueID(resultSet.getInt("venueid"));
+                venue.setVenue(resultSet.getString("venue"));
+                venue
+            }
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        }
+        
+    }
     
 }
